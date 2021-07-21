5c9d6f4d54beb0a06a5c7ec3a70f27d8f5a5b0f6
Amit <amit.kr8122001@gmail.com>
2021-07-21 11:47:43 +0530

    initial commit

---
+++ web d lab 4.sql
@@ -0,0 +1,19 @@
+<!DOCUMENT html>
+<html>
+<head>
+    <title>EMPLOYEE REGISTRATION FORM</title>
+    <link rel="css/style.css">
+</head>
+<h1 style="text-align: center">
+<u> EMLOYEE REGISTRATION FORM</u></h1>
+<h1 style="text-align: center">
+<u> session 2021-2022 </u></h1>
+<body style="text-align:center ;background-color: green ;"></body>
+<form>
+<p>
+<lable><strong>First Name</strong></lable>
+<br>
+<input text="text" placeholder="enter your name" size"80">
+</P>
+</form>
+</html>
